52c1754397afe397a135c75be6e1395858d93ee5
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-07-08 13:52:48 +0530

    Added the App components

--- ShoppingAppWithOutContext/src/App.js
+++ ShoppingAppWithOutContext/src/App.js
@@ -1,1 +1,14 @@
-import "./components/index.css";
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client.js";
+import Header from "./components/Header.js";
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
